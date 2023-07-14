de968add6501a0f9e0f62e3a32f33c182965c325
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-15 00:52:47 +0100

    displaying list of names before sorting as buttons

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -1,8 +1,16 @@
 import { babyNameData } from "../utils/babyNameData";
 export function BabyName(): JSX.Element {
+
+const listOfNames = babyNameData.map((object) => <button key = {object.id}>{object.name}</button>)
+
+ 
+
+
   return (
     <>
       <h1>Baby Name App</h1>
+      <div>{listOfNames}</div>
+
     </>
   );
 }
