e33097d8afb1e62647670cca1018e37ea698d278
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-15 15:14:19 +0100

    added compareName to sortNames variable

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -1,16 +1,19 @@
 import { babyNameData } from "../utils/babyNameData";
+import { compareNames } from "../utils/compareNames";
+
 export function BabyName(): JSX.Element {
+  const listOfNames = babyNameData.map((object) => (
+    <button key={object.id}>{object.name}</button>
+  ));
 
-const listOfNames = babyNameData.map((object) => <button key = {object.id}>{object.name}</button>)
+  const sortNames = babyNameData.sort(compareNames);
 
- 
-
+  console.log(sortNames);
 
   return (
     <>
       <h1>Baby Name App</h1>
       <div>{listOfNames}</div>
-
     </>
   );
 }
