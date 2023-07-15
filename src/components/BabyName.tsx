eafc42eb67c3eceb91ba590438109a7ac00ef998
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-15 15:58:33 +0100

    conditionally assigned CSS class to buttons of baby names

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -1,9 +1,10 @@
 import { babyNameData } from "../utils/babyNameData";
 import { ascOrder } from "../utils/ascOrder";
+import "./BabyApp.css";
 
 export function BabyName(): JSX.Element {
-  const listOfNames = babyNameData.map((object) => (
-    <button key={object.id}>{object.name}</button>
+  const listOfNames = babyNameData.map((baby) => (
+    <button className = {baby.sex === "f"? "femaleName": "maleName"}key={baby.id}>{baby.name}</button>
   ));
 
   const sortNames = babyNameData.sort(ascOrder);
