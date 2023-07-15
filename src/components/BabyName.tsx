ab6c1139ee31d177579e9b16f6e1f441e9ab32db
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-15 18:06:09 +0100

    added inputName useState and input field

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { babyNameData } from "../utils/babyNameData";
 import { ascOrder } from "../utils/ascOrder";
 import "./BabyApp.css";
 
 export function BabyName(): JSX.Element {
+const [inputName, setInputName]= useState<string>();
+
+
+
   const buttonsOfNames = babyNameData.map((baby) => (
     <button
       className={baby.sex === "f" ? "femaleName" : "maleName"}
@@ -19,6 +24,11 @@ export function BabyName(): JSX.Element {
   return (
     <>
       <h1>Baby Name App</h1>
+      <input
+      placeholder="Search Baby Name"
+      onChange={(e) => setInputName(e.target.value)}
+      value={inputName}
+      ></input>
       <div>{buttonsOfNames}</div>
     </>
   );
