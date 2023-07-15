1184890e819042a4764c06a71784fb0c875eefef
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-15 18:00:17 +0100

    centered h1 text and added background colour

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -3,7 +3,7 @@ import { ascOrder } from "../utils/ascOrder";
 import "./BabyApp.css";
 
 export function BabyName(): JSX.Element {
-  const listOfNames = babyNameData.map((baby) => (
+  const buttonsOfNames = babyNameData.map((baby) => (
     <button
       className={baby.sex === "f" ? "femaleName" : "maleName"}
       key={baby.id}
@@ -19,7 +19,7 @@ export function BabyName(): JSX.Element {
   return (
     <>
       <h1>Baby Name App</h1>
-      <div>{listOfNames}</div>
+      <div>{buttonsOfNames}</div>
     </>
   );
 }
