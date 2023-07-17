b84eb058a1deeb659f8ec6f25a8457effdab0f13
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-17 22:38:40 +0100

    formatted buttonId file

--- src/utils/buttonId.ts
+++ src/utils/buttonId.ts
@@ -1,21 +1,15 @@
-interface Button{
-    name: string,
-    gender: string,
-    icon: string,
+interface Button {
+  name: string;
+  gender: string;
+  icon: string;
 }
 
-
-
-export function buttonId(buttonData: Button): string{
-    if (buttonData.gender === "f") {
-        return "girlButton";
-       
-      } else if (buttonData.gender === "m") {
-        return "boyButton";
-        
-      } else {
-        return "allButton";
-       
-      }
-
+export function buttonId(buttonData: Button): string {
+  if (buttonData.gender === "f") {
+    return "girlButton";
+  } else if (buttonData.gender === "m") {
+    return "boyButton";
+  } else {
+    return "allButton";
+  }
 }
