aae213a4c890b487fd8bc064de95dfed00499d77
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-17 22:13:23 +0100

    changed the relative path to the icon images

--- src/utils/filterButtonsData.ts
+++ src/utils/filterButtonsData.ts
@@ -1,16 +1,16 @@
 export const filterButtons = [{
     name: "Girls",
     gender: "f",
-    icon: "../public/buttonIcons/pink-bird.png",
+    icon: "./buttonIcons/pink-bird.png",
 },
 {
     name: "Boys",
     gender: "m",
-    icon: "../public/buttonIcons/blue-bird.png", 
+    icon: "./buttonIcons/blue-bird.png", 
 },
 {
     name: "All",
     gender: "all",
-    icon: "../public/buttonIcons/love-birds.png", 
+    icon: "./buttonIcons/love-birds.png", 
 }];
 
