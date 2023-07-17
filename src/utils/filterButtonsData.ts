5f3248c71332306da5ed19c338bb010362219cf6
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-17 20:40:04 +0100

    added filterButtonsData

---
+++ src/utils/filterButtonsData.ts
@@ -0,0 +1,15 @@
+export const filterButtons = [{
+    name: "Girls",
+    gender: "f",
+    icon: "./public/buttonIcons/pink-bird.png",
+},
+{
+    name: "Boys",
+    gender: "f",
+    icon: "./public/buttonIcons/blue-bird.png", 
+},
+{
+    name: "All",
+    gender: "all",
+    icon: "./public/buttonIcons/love-birds.png", 
+}];
